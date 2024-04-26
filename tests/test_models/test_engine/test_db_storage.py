d838610a2269dc9bed47cba13f53c99035d8a977
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-26 17:52:46 +0000

    Add tests for count and get methods in DBStorage class

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,10 +3,12 @@
 Contains the TestDBStorageDocs and TestDBStorage classes
 """
 
+from os import getenv
 import inspect
 import unittest
 import pep8
 import models
+import models.base_model
 from models.engine import db_storage
 from models.amenity import Amenity
 from models.city import City
@@ -97,3 +99,55 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != "db", "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+
+@unittest.skipUnless(getenv('HBNB_TYPE_STORAGE') == "db", "testing DBStorage")
+class TestDBStorage(unittest.TestCase):
+    """Tests the DBStorage."""
+
+    def setUp(self) -> None:
+        models.storage.drop_all_tables()
+
+    def test_count_when_empty(self):
+        """Test that the `count` method returns zero when nothing exist."""
+        self.assertTrue(models.storage.count() == 0)
+
+    def test_count_all_objects(self):
+        """Test that the `count` method returns the right number of objects."""
+        for i in range(1, 11):
+            state = State(name=f"State_{i}")
+            state.save()
+            City(name=f"City_{i}", state_id=state.id).save()
+
+        self.assertEqual(models.storage.count(), 20)
+
+    def test_count_with_model_name(self):
+        """Test that the `count` method returns the right number of objects for
+        a particular class."""
+        State(name="Arizona").save()
+        State(name="California").save()
+
+        self.assertEqual(models.storage.count(State), 2)
+
+    def test_get_with_non_existent(self):
+        """Test that `get` method returns None for non-existent objects."""
+        self.assertIsNone(models.storage.get(User, 'abcd-1234-test-5678'))
+
+    def test_get_with_class_only(self):
+        """Test that the `get` method operates correctly when only the class
+        argument is passed."""
+        self.assertIsNone(models.storage.get(User))
+
+    def test_get_with_valid_class(self):
+        """Test that `get` method returns the right object."""
+        state = State(name="Greater Accra")
+        state.save()
+
+        # test for state instance
+        self.assertEqual(models.storage.get(
+            State, state.id), state)
+
+        city = City(name="Tema", state_id=state.id)
+        city.save()
+        self.assertEqual(models.storage.get(
+            City, city.id), city)
