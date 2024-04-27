22df5ff64b59f402ca1ca4f45cb43bac7ea8f87a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-27 18:10:31 +0000

    Refactor app.py to include error handling for 404 page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """This module contains routes for the app."""
 
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -19,9 +19,15 @@ def close(_):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(_):
+    """Returns an error 404 for page not found errors."""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     app.run(
-        host=f"{os.getenv('HBNB_API_HOST', default='0.0.0.0')}",
-        port=f"{os.getenv('HBNB_API_PORT', default='5000')}",
+        host=os.getenv('HBNB_API_HOST', default='0.0.0.0'),
+        port=int(os.getenv('HBNB_API_PORT', default='5000')),
         threaded=True
     )
