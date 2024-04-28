7ffb5396485b542ba8aeee6c433769dce72e116f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-28 08:08:05 +0000

    Fix missing save() calls in DBStorage and FileStorage classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,7 +31,7 @@ class FileStorage:
         if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
+                if cls in [value.__class__, value.__class__.__name__]:
                     new_dict[key] = value
             return new_dict
         return self.__objects
@@ -67,6 +67,7 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+                self.save()
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
